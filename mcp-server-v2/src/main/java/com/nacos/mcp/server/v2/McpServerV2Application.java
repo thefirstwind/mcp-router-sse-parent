ee19c4be5d973b5b55cdb9cddcf536876200a96f
shine <shine@shinedeMacBook-Air.local>
2025-07-15 09:03:58 +0800

    finish mcp-server auto configuration ***自动配置***

--- mcp-server-v2/src/main/java/com/nacos/mcp/server/v2/McpServerV2Application.java
+++ mcp-server-v2/src/main/java/com/nacos/mcp/server/v2/McpServerV2Application.java
@@ -1,16 +1,15 @@
 package com.nacos.mcp.server.v2;
 
 import com.alibaba.cloud.ai.autoconfigure.mcp.server.NacosMcpGatewayAutoConfiguration;
+import com.alibaba.nacos.api.config.annotation.NacosProperty;
+import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import com.nacos.mcp.server.v2.tools.PersonManagementTool;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-//@SpringBootApplication
-// @EnableDiscoveryClient
 @SpringBootApplication(exclude = NacosMcpGatewayAutoConfiguration.class)
 public class McpServerV2Application {
 
@@ -18,8 +17,4 @@ public class McpServerV2Application {
         SpringApplication.run(McpServerV2Application.class, args);
     }
 
-//    @Bean
-//    public ToolCallbackProvider tools(PersonManagementTool personManagementTool) {
-//        return MethodToolCallbackProvider.builder().toolObjects(personManagementTool).build();
-//    }
-} 
+}
